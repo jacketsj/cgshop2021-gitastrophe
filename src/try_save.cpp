983d1f973051474bc51b933d4c07efb509c62c2e
zhengdw <zhengdw@alumni.ubc.ca>
2021-05-10 20:36:49 -0500

    cleanup

--- src/try_save.cpp
+++ src/try_save.cpp
@@ -2,7 +2,6 @@
 #include <string>
 
 #include "Instance.h"
-#include "ThreadPool.h"
 
 using std::cin;
 using std::string;
@@ -17,13 +16,7 @@ void run(string name) {
 
 int main(int argc, char* argv[]) {
 	string name;
-	thread_pool tp(8);
 	while (cin >> name) {
-			{
-				unique_lock l(tp.m);
-				tp.qfull.wait(l, [&tp]() { return tp.q.size() < tp.QSZ; });
-			}
-			packaged_task<void()> p(bind(run, name));
-			tp.add(move(p));
+      run(name);
 	}
 }

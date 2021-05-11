477319e4e5990e1a77fd7b4d527274ee6af8d50a
zhengdw <zhengdw@alumni.ubc.ca>
2021-05-10 19:37:51 -0500

    Removing scripts

--- build/parallel_lab_opt_kill.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for x in $(seq -f "%02g" 1 25)
-do
-		ssh lin"$x" "pkill -f parallel_k_opt" # -f is to match full name of process
-		sleep .1 	# this sleep is necessary to prevent sshing too quickly and having a conflict in lock file
-done
-
--- build/parallel_lab_probe.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for x in $(seq -f "%02g" 1 25)
-do
-		ssh lin$x "echo 'Im alive!' &> >(trap '' INT TERM; sed 's/^/lin$x: /')" & # add prefix of machine it came from
-		sleep .1
-done
-
